539fbb70a845fba77304582adeaef06e082a5d09
viet <vietdq2412@gmail.com>
2020-12-09 10:26:14 +0700

    d

--- src/main/java/com/codegym/casestudy4/CaseStudy4Application.java
+++ src/main/java/com/codegym/casestudy4/CaseStudy4Application.java
@@ -1,15 +1,18 @@
 package com.codegym.casestudy4;
 
 import com.codegym.casestudy4.model.AppUser;
+import com.codegym.casestudy4.service.product.ProductServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.parameters.P;
 
 @SpringBootApplication
 public class CaseStudy4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CaseStudy4Application.class, args);
-        AppUser appUser = new AppUser();
+        ProductServiceImpl productService = new ProductServiceImpl();
+        productService.findAll();
     }
 
 }
