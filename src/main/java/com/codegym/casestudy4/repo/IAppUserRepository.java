fde388aae5277d5e557055329cf99912562f5780
viet <vietdq2412@gmail.com>
2020-12-12 13:04:59 +0700

    cus-UI

--- src/main/java/com/codegym/casestudy4/repo/IAppUserRepository.java
+++ src/main/java/com/codegym/casestudy4/repo/IAppUserRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@EnableJpaRepositories
 public interface IAppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
 
